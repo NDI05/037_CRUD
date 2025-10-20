c09a69b2eb03ac00f11eb279c085cc913c562ad9
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-10-20 09:51:28 +0700

    add new get  endpoint

--- helpers/connectionDB.js
+++ helpers/connectionDB.js
@@ -1,9 +1,20 @@
 const mysql = require('mysql2')
 
-const db = mysql.connection({
+const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "samarinda",
     database: "mahasiswa",
     port: 3306
 })
+
+db.connect((err)=>{
+    if(err){
+        console.log('Error Connection Into MySql: '+err)
+        return;
+    }
+    console.log('Connection Successfully');
+    
+})
+
+module.exports = db;
--- server.js
+++ server.js
@@ -2,11 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+const db = require('./helpers/connectionDB')
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res)=>{
     res.json({message: "hello world"})
+})
+
+app.get('/api/users', (req, res) =>{
+    db.query('SELECT * FROM mahasiswa', (err, result) =>{
+        if(err){
+            console.error('Error executing query:0'+err.stack)
+            res.status(500).send('Error Fetching Users');
+            return;
+        }
+        res.json(result);
+    })
 })
 
 app.listen(port, ()=>{
