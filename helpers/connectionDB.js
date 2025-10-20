b5ce05b7383401782aee4ff3205ccda579eb13c8
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-10-20 09:27:44 +0700

    add gitignore

--- helpers/connectionDB.js
+++ helpers/connectionDB.js
@@ -1,1 +1,12 @@
 const mysql = require('mysql2')
+
+const connection = mysql.connection({
+    host: "localhost",
+    user: "root",
+    password: "samarinda",
+    database: "mahasiswa"
+})
+
+connection.connect((err)=>{
+    if (err) throw err
+})
